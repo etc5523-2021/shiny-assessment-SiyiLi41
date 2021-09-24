a33e503dd5f2a1ccfcb07a4dd6c80fab0e8b2cd2
Siyi Li <slii0048@student.monash.edu>
2021-09-24 17:45:03 +1000

    add the app.R file

--- app.R
+++ app.R
@@ -7,6 +7,18 @@ library(here)
 # once you've prepared the data uncomment this line
 tidy_fuels <- read_csv(here("data", "cooking.csv"))
 # you might want to use highlight_key here
+fuels<-tidy_fuels%>%
+  mutate(new_cooking=noquote(format(cooking, digits=2, nsmall=2)),
+         new_gdp_per_capita=noquote(format(gdp_per_capita, digits=2, nsmall=2)),
+         population=formatC(tidy_fuels$total_population, format="d", big.mark=","))%>%
+  select(-c(cooking:total_population))
+
+
+countryset<-tidy_fuels%>%
+  distinct(country) %>%
+  arrange(country) %>%
+  pull(country)
+
 
 ui <- fluidPage(
   title = "Indoor Air Pollution",
@@ -26,16 +38,17 @@ ui <- fluidPage(
           offset = 1,
           # also possible to use plotly here
           selectizeInput("countries", "Select Countries",
-            choices = NULL,
-            multiple = TRUE
-          )
+                         multiple = TRUE,
+                         label="Which country do you want to analyze:",
+                         choices=countryset
+          ),
         ),
         column(
           2,
           offset = 1,
           checkboxInput("small_countries",
             "Hide countries < 1 million",
-            value = FALSE
+            value =NULL
           )
         )
       ),
@@ -50,22 +63,159 @@ ui <- fluidPage(
       )
     ),
     tabPanel("table", dataTableOutput("table"), icon = icon("table")),
-    tabPanel("about", icon = icon("question"))
+    tabPanel("about", icon = icon("question"), uiOutput("tab"))
   )
 )
 
-server <- function(input, output, session) {
-  # Define reactive expressions here for filtering data
 
-  # Define outputs here
-  output$chart <- renderPlotly({
-    ggplotly(ggplot() +
-      geom_blank())
-  })
+
+
+server <- function(input, output, session) ({
+
+
+  newdata <- reactive(
+    if(input$small_countries){
+      tidy_fuels%>%
+        group_by(continent)%>%
+        filter(year==input$year)%>%
+        filter(total_population > 1000000)
+    }else{
+      tidy_fuels%>%
+        group_by(continent)%>%
+        filter(year==input$year)
+      })
+
+  # selection<- reactiveValues(countries=NULL)
+  #
+  # observeEvent(input$countries,{
+  #   selection$countries<-input$countries
+  #   })
+
+  data<-reactive(
+  if(length(input$countries)==0){
+    newdata()
+  }else{
+    newdata()%>%
+      filter(country %in% input$countries)
+    }
+  )
+
+  # data <- reactive({
+  #   req(input$countries)
+  #   newdata()%>%
+  #        filter(country %in% input$countries)
+  # })
+
+  output$chart<- renderPlotly({
+
+# plotly::highlight(selectize = TRUE)
+    p<- ggplot(data = highlight_key(newdata(), ~country),
+              aes(x=gdp_per_capita,
+                  y=cooking,
+                  label=country,
+                  color = continent,
+                  size=5))+
+      geom_point()+
+      scale_size_continuous(trans = "log10") +
+      scale_y_continuous(labels = scales::label_percent(scale = 1)) +
+      labs(x = "GDP per captia",
+           y = "Access to clean fuels and technologies for cooking",
+           color = "continent",
+           title = "Access to clean fuels for cooking vs. GDP per capita")+
+      geom_point(
+      data = data() ,
+      aes(x=gdp_per_capita,
+          y=cooking,
+          size= 100)
+      )
+
+
+    # p1<-ggplot()+
+    #   geom_point(
+    #   data = data(),
+    #   aes(x=gdp_per_capita,
+    #       y=cooking,
+    #       size= cooking))
+
+    # if(length(input$countries)==0){
+    #   p
+    #   }else{ p1
+
+          # inherit.aes = FALSE
+
+
+    if(input$linear_scale){
+      p2<- ggplotly(p,
+                    tooltip = c("country","cooking","gdp_per_capita","total_ population"))
+      p3<-p2 %>%
+        config(displaylogo = FALSE,
+               modeBarButtonsToRemove = c("zoomIn2d", "zoomOut2d", "zoom2d", "pan2d"))
+
+
+      highlight(p3, dynamic = TRUE, on = "plotly_hover")
+    }
+
+    else{
+      p4<- ggplotly(p + scale_x_log10(c(1000, 2000, 5000, 10000, 20000, 50000, 100000),
+                                     labels = ~scales::dollar(., accuracy=1)),
+                    tooltip = c("country","cooking","gdp_per_capita","total_ population"))
+      p5<-p4 %>%
+        config(displaylogo = FALSE,
+               modeBarButtonsToRemove = c("zoomIn2d", "zoomOut2d", "zoom2d", "pan2d"))
+
+
+      p5
+
+    }
+})
+
+
+
+
+  data1<- reactive(
+    if(input$small_countries){
+      fuels%>%
+        group_by(year)%>%
+        arrange(-year)%>%
+        filter(population > 1000000)
+
+    }else{
+      fuels%>%
+       arrange(-year)
+    })
+
+
+
+
+  data2<-reactive(
+    if(length(input$countries)==0){
+      data1()
+       }else{
+      data1()%>%
+        filter(country %in% input$countries)%>%
+        arrange(-year)
+    })
 
   output$table <- renderDataTable({
-    NULL
+
+    DT::datatable(
+                  data2(),
+                  options = list(
+                    lengthMenu = c(5, 30, 50), pageLength = 10))%>%
+      formatStyle(columns = c("continent", "country","code","year"), `text-align` = 'left') %>%
+      formatStyle(columns = c("population", "new_cooking","new_gdp_per_capita"), `text-align` = 'right')
   })
-}
+
+  ur1<-a("github", href="https://github.com/SiyiLi41")
+  ur2<-a("source", href="https://ourworldindata.org/grapher/access-to-clean-fuels-for-cooking-vs-gdp-per-capita")
+  output$tab<-renderUI({
+    tagList("URL:",ur1,
+            br(),br(),br(),
+            "URL:",ur2)
+  })
+
+
+
+})
 
 runApp(shinyApp(ui, server))
